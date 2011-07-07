b06b625721c03691b08e659428405699111cd544
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-07-07 11:39:52 +0200

    Add export symbols

--- SWIG/string/myfile.h
+++ SWIG/string/myfile.h
@@ -1,4 +1,14 @@
-class myfile
+#ifdef _WIN32
+  #if defined(thelib_EXPORTS) || defined(demo_EXPORTS)
+    #define MY_EXPORT __declspec( dllexport )
+  #else
+    #define MY_EXPORT __declspec( dllimport )
+  #endif
+#else
+  #define MY_EXPORT
+#endif
+
+class MY_EXPORT myfile
 {
 public:
   static bool open( const char *filename );

529a5e65e2e8b5299b558effbf19ce92e32f3b29
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-10-07 09:54:40 +0200

    Import upstream version

---
+++ debian/cableswig/debian/makeSource.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+set -e
+
+VERSION=0.1.0
+ITKBRANCHTAG=ITK-3-20
+DATE=20110226
+
+cvsdirname=`pwd`
+dirname=cableswig-${VERSION}+cvs${DATE}.orig
+tarname=cableswig_${VERSION}+cvs${DATE}.orig.tar.gz
+
+cvs -z3 export -r ${ITKBRANCHTAG} -D ${DATE} -d ${dirname} CableSwig
+
+# Remove files with (a) no license, or (b) non-free license.
+#
+rm -rf ${dirname}/GCC
+rm -rf ${dirname}/GCC_XML
+
+
+# Build tarball
+#
+rm -rf ../${dirname}
+mv ${dirname} ..
+cd ..
+tar czf ${tarname} ${dirname}

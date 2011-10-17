bcb80ff33f09b5a5e6e6878675f39699d43ecc31
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-10-17 09:20:51 +0200

    Change layout

--- VTK/wl/ImageInteractorReslice.py
+++ VTK/wl/ImageInteractorReslice.py
@@ -20,40 +20,61 @@ reader.SetDataExtent(0,63,0,63,1,93)
 reader.SetDataSpacing(3.2,3.2,1.5)
 reader.SetFilePrefix("" + str(VTK_DATA_ROOT) + "/Data/headsq/quarter")
 
-# Create the RenderWindow, Renderer
-ren1 = vtk.vtkRenderer()
 renWin = vtk.vtkRenderWindow()
-renWin.AddRenderer(ren1)
 iren = vtk.vtkRenderWindowInteractor()
 iren.SetRenderWindow(renWin)
 
-im = vtk.vtkImageResliceMapper()
-im.SetInputConnection(reader.GetOutputPort())
-im.SliceFacesCameraOn()
-im.SliceAtFocalPointOn()
-im.BorderOff()
+for i in range(0,3):
+  print i
+  # Create the RenderWindow, Renderer
+  ren1 = vtk.vtkRenderer()
+  ren1.SetBackground(0.1,0.2,0.4);
+  if i == 0:
+    ren1.SetViewport(0., 0., 0.333, 1);
+  elif i == 1:
+    ren1.SetViewport(0.333, 0., 0.667, 1);
+  elif i == 2:
+    ren1.SetViewport(0.667, 0., 1., 1.);
+  renWin.AddRenderer(ren1)
+  
+  im = vtk.vtkImageResliceMapper()
+  im.SetInputConnection(reader.GetOutputPort())
+  im.SliceFacesCameraOn()
+  im.SliceAtFocalPointOn()
+  im.BorderOff()
+  
+  ip = vtk.vtkImageProperty()
+  ip.SetColorWindow(2000)
+  ip.SetColorLevel(1000)
+  ip.SetAmbient(0.0)
+  ip.SetDiffuse(1.0)
+  ip.SetOpacity(1.0)
+  ip.SetInterpolationTypeToLinear()
+  
+  bounds = im.GetBounds()
+  print bounds
+  point = [ 0.5*(bounds[0] + bounds[1]),
+  0.5*(bounds[2] + bounds[3]),
+  0.5*(bounds[4] + bounds[5])
+  ]
+  print point
 
-ip = vtk.vtkImageProperty()
-ip.SetColorWindow(2000)
-ip.SetColorLevel(1000)
-ip.SetAmbient(0.0)
-ip.SetDiffuse(1.0)
-ip.SetOpacity(1.0)
-ip.SetInterpolationTypeToLinear()
+  camera = ren1.GetActiveCamera()
+  camera.SetFocalPoint(point)
+  point[2] += 50.0
+  camera.SetPosition(point)
+  camera.ParallelProjectionOn()
+  camera.SetParallelScale(5.0)
 
-ia = vtk.vtkImageSlice()
-ia.SetMapper(im)
-ia.SetProperty(ip)
+  ia = vtk.vtkImageSlice()
+  ia.SetMapper(im)
+  ia.SetProperty(ip)
+  
+  ren1.AddViewProp(ia)
+  ren1.SetBackground(0.1,0.2,0.4)
+  renWin.SetSize(300,300)
+  
 
-ren1.AddViewProp(ia)
-ren1.SetBackground(0.1,0.2,0.4)
-renWin.SetSize(300,300)
-
-iren = vtk.vtkRenderWindowInteractor()
-style = vtk.vtkInteractorStyleImage()
-style.SetInteractionModeToImage3D()
-
-# Rotate the puzzle
 def CharCallback(obj, event):
     iren = renWin.GetInteractor()
     keycode = iren.GetKeyCode()
@@ -65,6 +86,9 @@ def CharCallback(obj, event):
         #style.EndSpin();
         style.EndWindowLevel();
         return
+
+style = vtk.vtkInteractorStyleImage()
+style.SetInteractionModeToImage3D()
 
 iren.SetInteractorStyle(style)
 iren.AddObserver("CharEvent", CharCallback)

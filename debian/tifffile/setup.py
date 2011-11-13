2c616b112b8589ec5ed5273cf28731d2957c2d25
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2011-11-13 19:13:27 +0100

    fixup

--- debian/tifffile/setup.py
+++ debian/tifffile/setup.py
@@ -7,5 +7,11 @@ Usage:: ``python setup.py build_ext --inplace``
 from distutils.core import setup, Extension
 import numpy
 
-setup(name='_tifffile', ext_modules=[Extension('_tifffile', ['tifffile.c'],
-    include_dirs=[numpy.get_include()], extra_compile_args=[])],)
+setup(name='_tifffile',
+    ext_modules=[
+      Extension(
+        '_tifffile', ['tifffile.c'],
+        include_dirs=[numpy.get_include()],
+        extra_compile_args=[])],
+  py_modules=["tifffile"],
+)

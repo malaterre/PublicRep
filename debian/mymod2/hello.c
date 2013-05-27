6399c730122e3ddfdbcb38f7210cfb1f40dfc198
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2013-05-27 18:08:39 +0200

    more

--- debian/mymod2/hello.c
+++ debian/mymod2/hello.c
@@ -1,20 +1,21 @@
 /* http://www.linuxchix.org/content/courses/kernel_hacking/lesson8 */
-#include <linux/module.h>
 #include <linux/init.h>
+#include <linux/module.h>
+MODULE_LICENSE("GPL");
 
-static int __init hello_init(void)
+static int hello_init(void)
 {
- pr_err("My module worked!\n");
+ //pr_err("My module worked!\n");
+ printk(KERN_ALERT "My module worked!\n");
         return 0;
 }
 
-static void __exit hello_exit(void)
+static void hello_exit(void)
 {
- pr_err("Unloading my module.\n");
+// pr_err("Unloading my module.\n");
+ printk(KERN_ALERT "My module unloaded!\n");
         return;
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
-
-MODULE_LICENSE("GPL");

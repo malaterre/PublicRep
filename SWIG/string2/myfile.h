0020547ef674a1e6ddeb62c6ccda7c1b0404e924
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2019-07-05 17:30:48 +0200

    first shot

---
+++ SWIG/string2/myfile.h
@@ -0,0 +1,15 @@
+#ifdef _WIN32
+  #if defined(thelib_EXPORTS) || defined(demo_EXPORTS)
+    #define MY_EXPORT __declspec( dllexport )
+  #else
+    #define MY_EXPORT __declspec( dllimport )
+  #endif
+#else
+  #define MY_EXPORT
+#endif
+
+class MY_EXPORT myfile
+{
+public:
+  static bool open( const char *filename );
+};
---
+++ SWIG/string2/testdemo.cs
@@ -0,0 +1,58 @@
+import demo.*;
+import java.io.*;
+
+/*
+ * usage:
+ * mkdir yeeha
+ * LD_LIBRARY_PATH=. CLASSPATH=demo.jar:. java testdemo yeeha
+ */
+
+public class testdemo
+{
+  public static void process(File file)
+    {
+    String path = file.getPath();
+    myfile mf = new myfile();
+    boolean res = mf.open( path );
+    }
+
+  public static void visitAllFiles(File dir)
+    {
+    if (dir.isDirectory())
+      {
+      String[] children = dir.list();
+      for (int i=0; i<children.length; i++)
+        {
+        visitAllFiles(new File(dir, children[i]));
+        }
+      }
+    else
+      {
+      process(dir);
+      }
+    }
+
+  public static void createNewEmptyFile(String dir)
+    {
+    File file = new File( dir, "tête.txt");
+    boolean b = false;
+    try
+      {
+      b = file.createNewFile();
+      }
+    catch(IOException e)
+      {
+      System.out.println("Error createNewFile:" + e);
+      }
+    }
+
+  public static void main(String[] args) throws Exception
+    {
+    String directory = args[0];
+
+    createNewEmptyFile(directory);
+
+    File dir = new File(directory);
+    visitAllFiles(dir);
+    }
+}

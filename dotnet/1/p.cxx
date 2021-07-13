02e9c8744f25ed2a37af56ef1b66cc84081bae62
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2021-07-13 14:20:59 +0200

    simplify a bit

--- dotnet/1/p.cxx
+++ dotnet/1/p.cxx
@@ -1,32 +1,26 @@
-#include <string>
-
 namespace ns {
 class c {
  public:
-  c():S("42") {}
-  const std::string& get() const { return S; }
+  c():I(42) {}
 
  private:
-  std::string S;
+  int I;
 };
 }  // namespace ns
 
 extern "C" {
 typedef struct c c;
 
-__attribute__((visibility("default"))) c *
-ns_create() {
+__attribute__((visibility("default"))) c * ns_create() {
   return reinterpret_cast<c*>(new ns::c);
 }
 
-__attribute__((visibility("default"))) void ns_delete(
-    c *c) {
+__attribute__((visibility("default"))) void ns_delete(c *c) {
   delete reinterpret_cast<ns::c*>(c);
 }
 
-__attribute__((visibility("default"))) const char *
-ns_get(c *c) {
-  return "";
+__attribute__((visibility("default"))) const char * ns_get(c *c) {
+  return "42";
 }
 
 }  // end extern "C"

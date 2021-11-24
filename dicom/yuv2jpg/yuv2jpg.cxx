1a22903bec0848be17e96bab379d629f9618ea2a
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2021-11-24 15:59:02 +0100

    first commit

---
+++ dicom/yuv2jpg/yuv2jpg.cxx
@@ -0,0 +1,189 @@
+#include <stdio.h> // before jpeglib.h
+#include <jpeglib.h>
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <stdexcept>
+#include <iostream>
+
+class File {
+  typedef FILE *ptr;
+  ptr stream;
+
+ public:
+  File(std::string const &filename, std::string const &mode)
+      : stream(fopen(filename.c_str(), mode.c_str())) {}
+  ~File() {
+    if (stream) {
+      fclose(stream);
+    }
+  };
+  operator ptr() const { return stream; }
+  File(File const &) = delete;
+  File &operator=(File const &) = delete;
+};
+
+#if 0
+class Decompress {
+  typedef jpeg_decompress_struct *ptr;
+  jpeg_decompress_struct srcinfo{};
+
+ public:
+  Decompress() { jpeg_create_decompress(&srcinfo); }
+  ~Decompress() { jpeg_destroy_decompress(&srcinfo); }
+  operator ptr() { return &srcinfo; }
+  jpeg_decompress_struct *operator->() { return &srcinfo; }
+};
+#endif
+
+class Compress {
+  typedef jpeg_compress_struct *ptr;
+  jpeg_compress_struct dstinfo{};
+
+ public:
+  Compress() { jpeg_create_compress(&dstinfo); }
+  ~Compress() { jpeg_destroy_compress(&dstinfo); }
+  operator ptr() { return &dstinfo; }
+  jpeg_compress_struct *operator->() { return &dstinfo; }
+};
+
+
+int main(int argc, char *argv[])
+{
+  if( argc < 3 ) return 1;
+  const char *input = argv[1];
+  const char *output = argv[2];
+  int image_width = 555;
+  int image_height = 800;
+  int quality = 101;
+  JSAMPLE * image_buffer;	/* Points to large array of Y,Cb,Cr-order data */
+
+  std::ifstream ifs(input, std::ios::binary | std::ios::ate);
+  std::ifstream::pos_type pos = ifs.tellg();
+
+  std::vector<uint8_t> result(pos);
+  std::cout << pos << std::endl;
+  std::cout << image_width * image_height * 2 << std::endl;
+
+  ifs.seekg(0, std::ios::beg);
+  ifs.read(reinterpret_cast<char *>(result.data()), pos);
+  image_buffer = result.data();
+
+#if 0
+  File infile(input, "rb");
+  if (!infile) {
+    throw std::invalid_argument("input: " + input);
+  }
+    std::ifstream::pos_type pos = ifs.tellg();
+
+  std::vector<uint8_t> result(pos);
+
+  ifs.seekg(0, std::ios::beg);
+  ifs.read(reinterpret_cast<char *>(result.data()), pos);
+#endif
+
+  /* This struct contains the JPEG compression parameters and pointers to
+   * working space (which is allocated as needed by the JPEG library).
+   * It is possible to have several such structures, representing multiple
+   * compression/decompression processes, in existence at once.  We refer
+   * to any one struct (and its associated working data) as a "JPEG object".
+   */
+  Compress cinfo;
+  /* This struct represents a JPEG error handler.  It is declared separately
+   * because applications often want to supply a specialized error handler
+   * (see the second half of this file for an example).  But here we just
+   * take the easy way out and use the standard error handler, which will
+   * print a message on stderr and call exit() if compression fails.
+   * Note that this struct must live as long as the main JPEG parameter
+   * struct, to avoid dangling-pointer problems.
+   */
+  struct jpeg_error_mgr jerr;
+  JSAMPROW row_pointer[1];	/* pointer to JSAMPLE row[s] */
+  int row_stride;		/* physical row width in image buffer */
+
+  /* We have to set up the error handler first, in case the initialization
+   * step fails.  (Unlikely, but it could happen if you are out of memory.)
+   * This routine fills in the contents of struct jerr, and returns jerr's
+   * address which we place into the link field in cinfo.
+   */
+  cinfo->err = jpeg_std_error(&jerr);
+
+  /* Now we can initialize the JPEG compression object. */
+  jpeg_create_compress(cinfo);
+
+  /* Step 2: specify data destination (eg, a file) */
+  /* Note: steps 2 and 3 can be done in either order. */
+
+  /* Here we use the library-supplied code to send compressed data to a
+   * stdio stream.  You can also write your own code to do something else.
+   * VERY IMPORTANT: use "b" option to fopen() if you are on a machine that
+   * requires it in order to write binary files.
+   */
+  File outfile(output, "wb");
+  if (!outfile) {
+    throw std::invalid_argument("failure to open output.");
+  }
+  jpeg_stdio_dest(cinfo, outfile);
+
+  /* Step 3: set parameters for compression */
+
+  /* First we supply a description of the input image.
+   * Four fields of the cinfo struct must be filled in:
+   */
+  cinfo->image_width = image_width; 	/* image width and height, in pixels */
+  cinfo->image_height = image_height;
+  cinfo->input_components = 3;		/* # of color components per pixel */
+  cinfo->in_color_space = JCS_YCbCr; 	/* colorspace of input image */
+  /* Now use the library's routine to set default compression parameters.
+   * (You must set at least cinfo.in_color_space before calling this,
+   * since the defaults depend on the source color space.)
+   */
+  jpeg_set_defaults(cinfo);
+
+  /* Now you can set any non-default parameters you wish to.
+   * Here we just illustrate the use of quality (quantization table) scaling:
+   */
+  jpeg_set_quality(cinfo, quality, TRUE /* limit to baseline-JPEG values */);
+
+  /* Step 4: Start compressor */
+
+  /* TRUE ensures that we will write a complete interchange-JPEG file.
+   * Pass TRUE unless you are very sure of what you're doing.
+   */
+  jpeg_start_compress(cinfo, TRUE);
+
+  /* Step 5: while (scan lines remain to be written) */
+  /*           jpeg_write_scanlines(...); */
+
+  /* Here we use the library's state variable cinfo.next_scanline as the
+   * loop counter, so that we don't have to keep track ourselves.
+   * To keep things simple, we pass one scanline per call; you can pass
+   * more if you wish, though.
+   */
+  row_stride = image_width * 3;	/* JSAMPLEs per row in image_buffer */
+
+  while (cinfo->next_scanline < cinfo->image_height) {
+    /* jpeg_write_scanlines expects an array of pointers to scanlines.
+     * Here the array is only one element long, but you could pass
+     * more than one scanline at a time if that's more convenient.
+     */
+    row_pointer[0] = & image_buffer[cinfo->next_scanline * row_stride];
+    (void) jpeg_write_scanlines(cinfo, row_pointer, 1);
+  }
+
+  /* Step 6: Finish compression */
+
+  jpeg_finish_compress(cinfo);
+  /* After finish_compress, we can close the output file. */
+  //fclose(outfile);
+
+  /* Step 7: release JPEG compression object */
+
+  /* This is an important step since it will release a good deal of memory. */
+  //jpeg_destroy_compress(cinfo);
+
+  /* And we're done! */
+  std::cout << "success" << std::endl;
+  return 0;
+}

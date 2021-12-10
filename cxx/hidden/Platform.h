11b694fac0f6349ba7fef3c2d27b94d23ce0edb2
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2021-12-10 11:34:00 +0100

    vemo

--- cxx/hidden/Platform.h
+++ cxx/hidden/Platform.h
@@ -2,14 +2,6 @@
 #define OPENVDB_PLATFORM_HAS_BEEN_INCLUDED
 
 #define OPENVDB_EXPORT __attribute__((visibility("default")))
-#define OPENVDB_IMPORT __attribute__((visibility("default")))
-
-#ifdef OPENVDB_PRIVATE
-    #define OPENVDB_API OPENVDB_EXPORT
-#else
-    #define OPENVDB_API OPENVDB_IMPORT
-#endif
-
-
+#define OPENVDB_API OPENVDB_EXPORT
 
 #endif // OPENVDB_PLATFORM_HAS_BEEN_INCLUDED

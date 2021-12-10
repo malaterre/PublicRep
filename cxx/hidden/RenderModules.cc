1c3c5237ad7f1886a336b5e0a824f1d6fdb4a523
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2021-12-10 11:25:13 +0100

    rere

--- cxx/hidden/RenderModules.cc
+++ cxx/hidden/RenderModules.cc
@@ -1,21 +1,4 @@
-// Copyright Contributors to the OpenVDB Project
-// SPDX-License-Identifier: MPL-2.0
-
 #include "RenderModules.h"
-
-//#include <openvdb/points/PointDataGrid.h>
-//#include <openvdb/points/PointCount.h>
-//#include <openvdb/points/PointConversion.h>
-//#include <openvdb/tools/Morphology.h>
-//#include <openvdb/tools/Prune.h>
-//#include <openvdb/tree/LeafManager.h>
-//#include <openvdb/util/logging.h>
-//#include <algorithm> // for std::min()
-//#include <cmath> // for std::abs(), std::fabs(), std::floor()
-//#include <limits>
-//#include <stdexcept>
-//#include <type_traits> // for std::is_const
-
 
 namespace openvdb_viewer {
 
@@ -75,10 +58,6 @@ public:
 
 private:
 }; // TreeTopologyOp
-
-////////////////////////////////////////
-
-// Tree topology render module
 
 TreeTopologyModule::TreeTopologyModule(const openvdb::GridBase::ConstPtr& grid):
     mGrid(grid)

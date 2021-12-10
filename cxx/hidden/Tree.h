bc9ee3d9e3dbc68925ecea48f35ba6ff3352a79e
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2021-12-10 14:01:12 +0100

    dfd

--- cxx/hidden/Tree.h
+++ cxx/hidden/Tree.h
@@ -6,20 +6,16 @@
 class Tree
 {
 public:
-    static const std::string treeType();
+    static const std::string treeType() {
+      static std::once_flag once;
+      std::call_once(once, []()
+      {
+      });
+      return *sTreeTypeName;
+    }
     const std::string type() const { return this->treeType(); }
 private:
     static std::unique_ptr<const std::string> sTreeTypeName;
 };
 
 std::unique_ptr<const std::string> Tree::sTreeTypeName;
-
-inline const std::string
-Tree::treeType()
-{
-    static std::once_flag once;
-    std::call_once(once, []()
-    {
-    });
-    return *sTreeTypeName;
-}

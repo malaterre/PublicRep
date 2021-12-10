ed47e6823baa2f91cb379a36fc0cd0eeb7853c2c
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2021-12-10 11:36:47 +0100

    re

--- cxx/hidden/LeafNode.h
+++ cxx/hidden/LeafNode.h
@@ -2,10 +2,6 @@
 #define OPENVDB_TREE_LEAFNODE_HAS_BEEN_INCLUDED
 
 #include <Types.h>
-#include <iostream>
-#include <memory>
-#include <string>
-#include <type_traits>
 
 namespace openvdb {
 OPENVDB_USE_VERSION_NAMESPACE
@@ -16,12 +12,7 @@ template<typename T, Index Log2Dim>
 class LeafNode
 {
 public:
-    using BuildType = T;
     using ValueType = T;
-    using LeafNodeType = LeafNode<ValueType, Log2Dim>;
-    using Ptr = SharedPtr<LeafNode>;
-
-
 }; // end of LeafNode class
 
 } // namespace tree

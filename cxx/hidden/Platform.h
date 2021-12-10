cb74236ac12c4905d4081dc2a70789a68f51dffb
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2021-12-10 11:32:19 +0100

    vemo

--- cxx/hidden/Platform.h
+++ cxx/hidden/Platform.h
@@ -1,21 +1,9 @@
 #ifndef OPENVDB_PLATFORM_HAS_BEEN_INCLUDED
 #define OPENVDB_PLATFORM_HAS_BEEN_INCLUDED
 
-/// Helper macros for defining library symbol visibility
-#ifdef OPENVDB_EXPORT
-#undef OPENVDB_EXPORT
-#endif
-#ifdef OPENVDB_IMPORT
-#undef OPENVDB_IMPORT
-#endif
-#ifdef __GNUC__
-    #define OPENVDB_EXPORT __attribute__((visibility("default")))
-    #define OPENVDB_IMPORT __attribute__((visibility("default")))
-#endif
+#define OPENVDB_EXPORT __attribute__((visibility("default")))
+#define OPENVDB_IMPORT __attribute__((visibility("default")))
 
-#ifdef OPENVDB_API
-#undef OPENVDB_API
-#endif
 #ifdef OPENVDB_PRIVATE
     #define OPENVDB_API OPENVDB_EXPORT
 #else

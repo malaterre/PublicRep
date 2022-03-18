f45566b8862c06c7d54a2ddda788570d0bf9a7d0
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2022-03-18 16:07:31 +0100

    final release

--- dcmtk/2/icc.sh
+++ dcmtk/2/icc.sh
@@ -15,24 +15,32 @@ jpegtran -outfile img.jpg -copy none original.jpg
 # apply icc profile:
 jpgicc img_app2.jpg img_jpgicc.jpg
 
-img2dcm --vl-photo $opt img.jpg dcmtk.1.dcm
-img2dcm --vl-photo --keep-appn $opt img.jpg dcmtk.2.dcm
+# craft template:
+gdcmimg original.jpg template.dcm
 
-gdcmimg --template dcmtk.1.dcm img.jpg img.dcm
-gdcmimg --template dcmtk.1.dcm img_jpgicc.jpg img_jpgicc.dcm
+patientid=ICC1
+studyid=S1
+seriesnum=1
+instnum=1
+studydate=$(date '+%Y%m%d')
+studytime=$(date '+%H%M%S')
 
-gdcmimg --template dcmtk.1.dcm img_app2.jpg img_app2.dcm
-gdcmimg --template dcmtk.1.dcm img_app2.jpg img_app2_iccprofile.dcm
+opt="-df template.dcm -k InstanceNumber=$instnum -k SeriesNumber=$seriesnum -k StudyID=$studyid -k PatientID=$patientid -k StudyDate=$studydate -k StudyTime=$studytime --invent-type1 --insert-type2"
+
+img2dcm --vl-photo $opt img.jpg img.dcm
+img2dcm --vl-photo $opt img_jpgicc.jpg img_jpgicc.dcm
+img2dcm --vl-photo $opt img_app2.jpg img_app2.dcm
+img2dcm --vl-photo $opt img_app2.jpg img_app2_iccprofile.dcm
+
+#gdcmimg --template dcmtk.1.dcm img.jpg img.dcm
+#gdcmimg --template dcmtk.1.dcm img_jpgicc.jpg img_jpgicc.dcm
+#gdcmimg --template dcmtk.1.dcm img_app2.jpg img_app2.dcm
+#gdcmimg --template dcmtk.1.dcm img_app2.jpg img_app2_iccprofile.dcm
+
 dcmodify --no-backup --insert-from-file 0028,2000=profile.icm img_app2_iccprofile.dcm
 
-#patientid=ICC1
-#studyid=S1
-#seriesnum=1
-#instnum=1
-#studydate=$(date '+%Y%m%d')
-#studytime=$(date '+%H%M%S')
-#
-##opt="-df template.dcm -k InstanceNumber=$instnum -k SeriesNumber=$seriesnum -k StudyID=$studyid -k PatientID=$patientid -k StudyDate=$studydate -k StudyTime=$studytime --invent-type1 --insert-type2"
-#
+
 #
 ##dciodvfy icc.1.dcm
+rm icc.tar.gz 
+tar cvfz icc.tar.gz img.dcm  img_jpgicc.dcm img_app2.dcm  img_app2_iccprofile.dcm README.txt

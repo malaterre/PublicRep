2b10fd8844acc1b3ea3f232a1a2392747971af2c
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2022-04-07 11:37:08 +0200

    cleanup

--- jpegxl/pfmdump.cxx
+++ jpegxl/pfmdump.cxx
@@ -1,34 +1,43 @@
-#include <iostream>
 #include <fstream>
-#include <string>
+#include <iostream>
 #include <limits>
-int main(int argc, char * argv[])
-{
-  const char * filename = argv[1];
-  std::ifstream is( filename, std::ios::binary );
+#include <sstream>
+#include <string>
+int main(int argc, char *argv[]) {
+  const char *filename = argv[1];
+  std::ifstream is(filename, std::ios::binary);
   std::string str;
   std::getline(is, str);
   std::cerr << str << std::endl;
+  if (str != "PF" && str != "Pf")
+    return 1;
+  unsigned int ncomps = str == "PF" ? 3u : 1u;
   std::getline(is, str);
   std::cerr << str << std::endl;
   long width, height;
-{
- std::istringstream iss(str);
-  iss >> width;
-  iss >> height;
-
-}
+  {
+    std::istringstream iss(str);
+    iss >> width;
+    iss >> height;
+  }
   std::getline(is, str);
   std::cerr << str << std::endl;
-  size_t nfloats = 1920u * 1280 * 3;
+  {
+    double endian;
+    std::istringstream iss(str);
+    iss >> endian;
+    if (endian > 0)
+      return 1;
+  }
+  size_t nfloats = width * height * ncomps;
   std::cerr << nfloats << std::endl;
   float fmin, fmax;
   fmin = std::numeric_limits<float>::max();
   fmax = std::numeric_limits<float>::lowest(); // min
   double count = 0;
-  for( size_t i = 0; i < nfloats; ++i ) {
+  for (size_t i = 0; i < nfloats; ++i) {
     float f = -1;
-    is.read((char*)&f, sizeof f);
+    is.read((char *)&f, sizeof f);
     fmin = std::min(fmin, f);
     fmax = std::max(fmax, f);
     count += f;

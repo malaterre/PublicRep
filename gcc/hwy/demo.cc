86bc3b7a53c62db7928644711cfbb11c4e9f55fa
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2022-08-08 11:29:07 +0200

    sdfsd

--- gcc/hwy/demo.cc
+++ gcc/hwy/demo.cc
@@ -1,6 +1,5 @@
 #include "hwy/aligned_allocator.h"
 #include "hwy/highway.h"
-#include "hwy/print.h"
 
 bool BytesEqual2(const void *p1, const void *p2, const size_t size,
                  size_t *pos = nullptr);
@@ -8,15 +7,13 @@ bool BytesEqual2(const void *p1, const void *p2, const size_t size,
 namespace hwy {
 namespace detail {
 
-void AssertArrayEqual2(const void *expected_void,
-                       const void *actual_void, size_t N,
-                       const char *target_name, const char *filename,
-                       int line) {
+void AssertArrayEqual2(const void *expected_void, const void *actual_void,
+                       size_t N) {
   const uint8_t *expected_array =
       reinterpret_cast<const uint8_t *>(expected_void);
   const uint8_t *actual_array = reinterpret_cast<const uint8_t *>(actual_void);
   for (size_t i = 0; i < N; ++i) {
-    const void *expected_ptr = expected_array + i * 2 ;
+    const void *expected_ptr = expected_array + i * 2;
     const void *actual_ptr = actual_array + i * 2;
     if (!BytesEqual2(expected_ptr, actual_ptr, 2)) {
       abort();
@@ -30,14 +27,12 @@ namespace N_EMU128 {
 template <class D, typename T = TFromD<D>, class V = Vec<D>>
 HWY_INLINE void AssertVecEqual(D d, const T *expected, VecArg<V> actual,
                                const char *filename, const int line) {
-  const size_t N = Lanes(d);
+  const size_t N = 2;
   auto actual_lanes = hwy::AllocateAligned<T>(N);
   Store(actual, d, actual_lanes.get());
 
-  const auto info = hwy::detail::MakeTypeInfo<T>();
   const char *target_name = hwy::TargetName(HWY_TARGET);
-  hwy::detail::AssertArrayEqual2( expected, actual_lanes.get(), N,
-                                 target_name, filename, line);
+  hwy::detail::AssertArrayEqual2(expected, actual_lanes.get(), N);
 }
 } // namespace N_EMU128
 } // namespace hwy

69f44e1b3757a2399ceed40298e0332558188b58
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2022-08-08 11:47:29 +0200

    fsdd

--- gcc/hwy/bytes.cc
+++ gcc/hwy/bytes.cc
@@ -1,10 +1,8 @@
 #include <cstddef>
 #include <cstdint>
 
-bool BytesEqual2(const void *p1, const void *p2, const size_t size,
-                 size_t *pos) {
-  const uint8_t *bytes1 = reinterpret_cast<const uint8_t *>(p1);
-  const uint8_t *bytes2 = reinterpret_cast<const uint8_t *>(p2);
+bool BytesEqual2(const uint8_t *bytes1, const uint8_t *bytes2,
+                 const size_t size, size_t *pos) {
   for (size_t i = 0; i < size; ++i) {
     if (bytes1[i] != bytes2[i]) {
       if (pos != nullptr) {
--- gcc/hwy/demo.cc
+++ gcc/hwy/demo.cc
@@ -1,15 +1,11 @@
 #include "hwy/aligned_allocator.h"
 #include "hwy/highway.h"
 
-bool BytesEqual2(const void *p1, const void *p2, const size_t size,
+bool BytesEqual2(const uint8_t *p1, const uint8_t *p2, const size_t size,
                  size_t *pos = nullptr);
 
-namespace hwy {
-
-namespace N_EMU128 {
-
-template <class D, typename T, class V>
-void AssertVecEqual2(D d, const T *expected, const V &actual) {
+template <class D, class V>
+void AssertVecEqual2(D d, const uint16_t *expected, const V &actual) {
   const size_t N = 2;
   auto actual_lanes = hwy::AllocateAligned<uint16_t>(N);
   Store(actual, d, actual_lanes.get());
@@ -17,15 +13,13 @@ void AssertVecEqual2(D d, const T *expected, const V &actual) {
   const uint8_t *actual_array =
       reinterpret_cast<const uint8_t *>(actual_lanes.get());
   for (size_t i = 0; i < N; ++i) {
-    const void *expected_ptr = expected_array + i * 2;
-    const void *actual_ptr = actual_array + i * 2;
+    const uint8_t *expected_ptr = expected_array + i * 2;
+    const uint8_t *actual_ptr = actual_array + i * 2;
     if (!BytesEqual2(expected_ptr, actual_ptr, 2)) {
       abort();
     }
   }
 }
-} // namespace N_EMU128
-} // namespace hwy
 
 int main() {
   hwy::N_EMU128::FixedTag<uint16_t, 2> d;
@@ -39,5 +33,5 @@ int main() {
   expected_lanes[1] = 16383;
   hwy::N_EMU128::Vec128<uint16_t, 2> v = Load(d, in_lanes.get());
   hwy::N_EMU128::Vec128<uint16_t, 2> actual = MulHigh(v, v);
-  hwy::N_EMU128::AssertVecEqual2(d, expected_lanes, actual);
+  AssertVecEqual2(d, expected_lanes, actual);
 }
